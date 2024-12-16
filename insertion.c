bec80fd849c3c9d80f202fbab386774ffdc4474e
Ketan Singh <hackerrajaput6@gmail.com>
2024-12-16 20:26:51 +0530

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -5,30 +5,33 @@
 int main (){
 
     int size , element , pos , i ;
-
+        // Input the size of Array
     printf("Enter the size of array :\n");
         scanf("%d",&size);
 
     int arr[size];
-
+        // Input the element of Array
     printf("Enter the element of array :\n");
         for ( i = 0; i < size; i++)
         {
             scanf("%d",&arr[i]);
         }
-    
+         // This is original Array
     printf("Your  array is :\n");
         for ( i = 0; i < size; i++)
         {
             printf("%d\t",arr[i]);
         }
+        // Enter the position 
         printf("\nEnter the position where you want to insert :\n");
         scanf("%d",&pos);
+        // Check if position is Valid
         if(pos<1 || pos>size)
         {
             printf("Invalid position");
             return(1);
         }
+        // input the element for insert
     printf("Enter the element you want to insert :\n");
         scanf("%d",&element);
     
@@ -40,15 +43,19 @@ int main (){
     }
     arr[pos-1]=element;                 
 
-    printf("The array has updated ...\n");
+    printf("The array has updated .....\n");
         sleep(2);
         
-    printf("This is your new array  : \n");
+    printf("This is new, Array After Insertion: \n");
         for ( i = 0; i < size+1; i++)
         {
             printf("%d\t",arr[i]);
         }
         printf("\n");
+        {
+            printf("\nThank you!\n");
+            printf("Your insertion is Successful");
+        }
         
   return 0;
 }
